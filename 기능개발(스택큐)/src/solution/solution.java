201738b8cf5daecf2856fc3a39d19ef2c04a55e4
wsx9412 <wsx9412@naver.com>
2019-12-23 03:08:07 +0900

    add java

---
+++ 기능개발(스택큐)/src/solution/solution.java
@@ -0,0 +1,46 @@
+package solution;
+
+import java.util.*;
+
+class Solution {
+	public static void main(String[]args) {
+		Solution mySolution = new Solution();
+		int[] progresses = {93,30,55};
+		int[] speeds = {1,30,5};
+		mySolution.solution(progresses,speeds);
+	}
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> list = new LinkedList<Integer>();
+        Queue<Integer> speed = new LinkedList<Integer>();
+        List<Integer> result = new ArrayList<Integer>();
+        int day = 0;
+        int work = 0;
+        int endWork = 0;
+        for(int i = 0 ; i < progresses.length; i++){
+            list.offer(progresses[i]);
+            speed.offer(speeds[i]);
+        }
+        do{
+            work = list.peek() + (speed.peek() * day);
+            if(work >= 100){
+            	list.poll();
+            	speed.poll();
+            	endWork++;
+                if(list.isEmpty())
+                	result.add(endWork);
+            }else{
+                if(endWork != 0){
+                    result.add(endWork);
+                    endWork = 0;                    
+                }
+                day++;
+            }
+        }while(!list.isEmpty());
+        int[] answer = new int[result.size()];
+        for(int i=0; i<result.size();i++){
+            answer[i] = result.get(i);
+            System.out.println(result.get(i));
+        }
+        return answer;
+    }
+}
