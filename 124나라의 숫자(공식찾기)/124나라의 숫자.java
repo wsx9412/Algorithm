abe2c675d2c4d9376bebc08ead0401b81b96f820
wsx9412 <wsx9412@naver.com>
2019-12-25 02:50:12 +0900

    new Algorithm(124나라의 숫자)

---
+++ 124나라의 숫자(공식찾기)/124나라의 숫자.java
@@ -0,0 +1,21 @@
+class Solution {
+  public String solution(int n) {
+      String answer = "";
+      int rem = 0;
+      while(n > 0){
+          rem = n % 3;
+          n /= 3;
+          if(rem == 0){
+              n--; 
+              rem = 4;
+          }
+          if(rem == 3){
+              rem = 4;
+          }
+          answer = Integer.toString(rem) + answer;
+      }
+         
+      System.out.print(answer);
+      return answer;
+  }
+}
