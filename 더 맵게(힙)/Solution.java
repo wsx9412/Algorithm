49eb282c07f7c25450207f1b33c9a19859d47f30
wsx9412 <wsx9412@naver.com>
2019-12-29 21:57:40 +0900

    new 힙 알고리즘

---
+++ 더 맵게(힙)/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+        
+        int answer = 0;
+        
+        for(int i = 0; i<scoville.length; i++)
+            heap.add(scoville[i]);
+        
+        for(int i = 0; i<scoville.length; i++){          
+            if( heap.size()<=1&& heap.peek() < K){
+                answer = -1;    
+                break;
+            } 
+            if(heap.peek()<K){
+                int min = heap.poll();
+                int min2 = heap.poll();
+                int sco = min + (min2 * 2);
+                heap.add(sco);
+                answer++;
+            }
+            if(heap.peek() >= K)
+                break;      
+        }
+        
+        
+        return answer;
+    }
+}
